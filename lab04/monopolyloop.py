5c4729be8744d6a2ba1c42404049cca3f369c02d
gavhart <har19076@byui.edu>
2024-02-01 13:26:19 -0700

    done

--- lab04/monopolyloop.py
+++ lab04/monopolyloop.py
@@ -14,82 +14,95 @@
 #     It took me about 4 hours to complete this assignment.
 
 # Constants for game prices
-HOUSE_PRICE = 200  # Adjust as necessary
-HOTEL_PRICE = 400  # Assuming the same price for simplification; adjust as needed
-
-def input_int(message):
-    """Safely captures integer input."""
-    while True:
-        try:
-            return int(input(message))
-        except ValueError:
-            print("Please enter a valid integer.")
-
 def main():
-    print("Monopoly Hotel Purchase Decision Program")
     # Ask the user if they own a property on all three places.
     color_group = input("Do you own all the green properties? (y/n): ").lower()
 
-    # If they don't own all the locations, end the program. If they do, ask what they have on Pennsylvania Ave.
-    if color_group != "y":
-        print("You cannot purchase a hotel until you own all the properties of a given color group.")
-        return
+    # If they don't own all the locations end the program, if they do ask what they have on Pennsylvania Ave
+    if color_group == "n":
+        print("You cannot purchase a hotel until you own all the properties of",
+         "given color group.")
+    elif color_group == "y":
+        prompt_pa = int(input("What is on Pennsylvania Avenue? (0:nothing, 1:one house, ... 5:a hotel): "))
+    
+        # Ask what they have on Pennsylvania Ave, if 5 end. 
+        # If they have less then 4 ask about North Carolina Ave
+        if prompt_pa == 5:
+            print("You cannot purchase a hotel if the property already has one.")
+        elif prompt_pa < 5:
+            prompt_nc = int(input("What is on North Carolina Avenue? (0:nothing, 1:one house, ... 5:a hotel): "))
+            
+            # Ask what they have on North Carolina Ave, if 5 end. 
+            # If they have less then 4 ask about Pacific Ave.
+            if prompt_nc == 5:
+                print("Swap North Carolina's hotel with Pennsylvania's 4 houses.")
+            elif prompt_nc < 5:
+                prompt_pc = int(input("What is on Pacific Avenue? (0:nothing, 1 :one house, ... 5:a hotel): "))
 
-    prompt_pa = input_int("What is on Pennsylvania Avenue? (0:nothing, 1:one house, ... 5:a hotel): ")
+                # Ask what they have on Pacific Ave, if 5 end. 
+                # If not, check how many houses they need, if there are
+                # enough available at the bank, and if they have enough
+                # money.
+                if prompt_pc == 5:
+                    print("Swap Pacific's hotel with Pennsylvania's 4 houses.")
+                else:
+                    prompt_hotels = int(input("How many hotels are there to purchase? "))
+                    main_a(prompt_hotels, prompt_nc, prompt_pc, prompt_pa)
+        
+def main_a(prompt_hotels, prompt_nc, prompt_pc, prompt_pa):
+    """Return print statements based on how many houses need purchased
+    at each location along with the total cost and instructions of 
+    what to do with each piece.
 
-    if prompt_pa == 5:
-        print("You cannot purchase a hotel if the property already has one.")
-        return
+    Parameters
+        prompt_hotels: Number of hotels available to purchase
+        prompt_nc: Number of houses on North Carolina Ave
+        prompt_pc: Number of houses on Pacific Ave
+        prompt_pa: Number of houses on Pennsylvania Ave
 
-    prompt_nc = input_int("What is on North Carolina Avenue? (0:nothing, 1:one house, ... 5:a hotel): ")
-    prompt_pc = input_int("What is on Pacific Avenue? (0:nothing, 1:one house, ... 5:a hotel): ")
-    prompt_hotels = input_int("How many hotels are there to purchase? ")
+    Return: the volume of the cylinder
+    """
 
+    # Determine how many hotels there are to purchase, if zero
+    # end. Otherwise compute how many houses are needed on each
+    # location as well as the total houses and cost to purchase.
     if prompt_hotels == 0:
         print("There are not enough hotels available for purchase at this time.")
-        return
+    elif prompt_hotels > 0:
+        num_house_pc_need = 4 - prompt_pc
+        num_house_nc_need = 4 - prompt_nc
+        num_house_pa_need = 4 - prompt_pa
+        num_house_total_need = num_house_pc_need + num_house_nc_need + num_house_pa_need
+        total_money_need = num_house_total_need * 200 + 200
+        money = int(input("How much cash do you have to spend? "))
 
-    main_decision(prompt_nc, prompt_pc, prompt_pa, prompt_hotels)
+        # If the user doesn't have enough money, end.
+        # If they do figure out if there are enough houses
+        if total_money_need > money:
+            print("You do not have sufficient funds to purchase a hotel at this time.")
+        elif total_money_need <= money:
+            prompt_houses = int(input("How many houses are there to purchase? "))
 
-def main_decision(prompt_nc, prompt_pc, prompt_pa, prompt_hotels):
-    num_house_pc_need = 4 - prompt_pc
-    num_house_nc_need = 4 - prompt_nc
-    num_house_total_need = num_house_pc_need + num_house_nc_need
-    total_money_need = (num_house_total_need * HOUSE_PRICE) + HOTEL_PRICE
-    money = input_int("How much cash do you have to spend? ")
-
-    if total_money_need > money:
-        print("You do not have sufficient funds to purchase a hotel at this time.")
-        return
-
-    prompt_houses = input_int("How many houses are there to purchase? ")
-    if num_house_total_need > prompt_houses:
-        print("There are not enough houses available for purchase at this time.")
-        return
-
-    # Summary of user inputs
-    print("\nSummary of your inputs:")
-    print(f"- Own all green properties: Yes\n- Pennsylvania Avenue: {prompt_pa} houses\n- North Carolina Avenue: {prompt_nc} houses\n- Pacific Avenue: {prompt_pc} houses\n- Cash: ${money}\n- Houses to purchase: {prompt_houses}\n- Hotels to purchase: {prompt_hotels}")
-
-    # Decision making for hotel purchase
-    if num_house_nc_need > 0 and num_house_pc_need > 0:
-        decision_message = "A"
-    elif num_house_nc_need > 0:
-        decision_message = "B"
-    elif num_house_pc_need > 0:
-        decision_message = "C"
-    else:
-        decision_message = "D"
-
-    print_purchase_decision(decision_message, total_money_need, num_house_nc_need, num_house_pc_need)
-
-def print_purchase_decision(decision, total_money_need, num_house_nc_need, num_house_pc_need):
-    print(f"\nThis will cost ${total_money_need}.")
-    print("Purchase 1 hotel and {num_house_total_need} house(s).")
-    print("Put 1 hotel on Pennsylvania and return any houses to the bank.")
-    if decision in ["A", "B"]:
-        print(f"Put {num_house_nc_need} house(s) on North Carolina.")
-    if decision in ["A", "C"]:
-        print(f"Put {num_house_pc_need} house(s) on Pacific.")
+            # If the there aren't enough houses, end
+            # otherwise return the cost to purchase the
+            # cost and houses needed in each location.
+            if num_house_total_need > prompt_houses:
+                print("There are not enough houses available for purchase at this time.")
+            elif num_house_nc_need > 0 and num_house_pc_need > 0:
+                print(f"""\nThis will cost ${total_money_need}.
+    Purchase 1 hotel and {num_house_total_need} house(s).
+    Put 1 hotel on Pennsylvania and return any houses to the bank.
+    Put {num_house_nc_need} house(s) on North Carolina.
+    Put {num_house_pc_need} house(s) on Pacific.""")
+            elif num_house_nc_need > 0:
+                print(f"""\nThis will cost ${total_money_need}.
+    Purchase 1 hotel and {num_house_total_need} house(s).
+    Put 1 hotel on Pennsylvania and return any houses to the bank.
+    Put {num_house_nc_need} house(s) on North Carolina.""")
+            elif num_house_pc_need > 0:
+                print(f"""\nThis will cost ${total_money_need}.
+    Purchase 1 hotel and {num_house_total_need} house(s).
+    Put 1 hotel on Pennsylvania and return any houses to the bank.
+    Put {num_house_pc_need} house(s) on Pacific.""")
 
 main()
